da589c51faaec9985c715bee4216a32e02859585
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2022-10-05 12:00:57 +0300

    Initial Commit

---
+++ MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Vasiliy on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
